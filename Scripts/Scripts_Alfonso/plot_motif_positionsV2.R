7316da721049689ec8527caf82c62b2c61fa9906
Jose Barragán Sánchez-Lanuza <lanuza@Joses-iMac.home>
2021-06-01 18:20:44 +0200

    Probando sin color

--- Scripts/Scripts_Alfonso/plot_motif_positionsV2.R
+++ Scripts/Scripts_Alfonso/plot_motif_positionsV2.R
@@ -19,7 +19,7 @@ equidistribution <- function(number_nodes){
 
 plot_motif_positions <- function(motif_number){
   
-  motifs_raw_data <- read_csv("Data/Data_processing/Motifs_connections/motif_pattern_connections.csv")
+  motifs_raw_data <- read_csv("../Data/Data_processing/Motifs_connections/motif_pattern_connections.csv")
   
   motif_data <- motifs_raw_data %>% filter(motif_id == motif_number)
   
@@ -63,10 +63,10 @@ plot_motif_positions <- function(motif_number){
   
   ggplot(data=d_nodes, aes(x, y)) +
     scale_shape_identity() +
-    geom_segment(data = link_df, aes(x=xdown, y=ydown, xend = xup, yend = yup), size = 2, color = "grey")+
-    geom_point(aes(color = as.factor(label)),size=12)+
-    geom_text(aes(x, y , label = label),size=4)+
-    scale_color_brewer(palette = "Set2")+
+    geom_segment(data = link_df, aes(x=xdown, y=ydown, xend = xup, yend = yup), size = 1, color = "grey20")+
+    geom_point(aes(color = as.factor(label)),size=10, color = "azure2")+
+    geom_text(aes(x, y , label = label),size=3)+
+ #   scale_color_brewer(palette = "Set2")+
     xlim(-1.5,4)+ ylim(-0.5,1.5)+
     theme(panel.background = element_blank(),
           axis.text.x=element_blank(),

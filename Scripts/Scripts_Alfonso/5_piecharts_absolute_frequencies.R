c81ef99472eb4a0fc87b4d1bca84f0220f1d75e7
AlfAllenP <57431873+AlfAllenP@users.noreply.github.com>
2021-06-11 09:05:45 +0200

    Update 5_piecharts_absolute_frequencies.R

--- Scripts/Scripts_Alfonso/5_piecharts_absolute_frequencies.R
+++ Scripts/Scripts_Alfonso/5_piecharts_absolute_frequencies.R
@@ -243,4 +243,166 @@ ggplot() + geom_scatterpie(aes(x=x, y=y, group=position,r=5),
   labs(title = "Plant positions",fill=NULL)
 
 
+#################################
+# CREATE PIE-MOTIFS
+#################################
+
+equidistribution <- function(number_nodes){
+  
+  x_width <- 5.5/(number_nodes+1)
+  
+  x_result <- rep(-1.5,number_nodes)
+  
+  for(i in 1:number_nodes){
+    
+    x_result[i] <- x_result[i] + i*x_width
+    
+  }
+  
+  return(x_result)
+  
+}
+
+plot_pie_motif_positions <- function(motif_number){
+  motifs_raw_data <- read_csv("Data/Data_processing/Motifs_connections/motif_pattern_connections.csv")
+  
+  motif_data <- motifs_raw_data %>% filter(motif_id == motif_number)
+  
+  
+  # Create dataframe with node positions
+  
+  labdown = motif_data$plant %>% unique()
+  
+  
+  xdown = equidistribution(length(labdown))
+  
+  ydown = rep(0,length(labdown))
+  
+  labup = motif_data$pollinator %>% unique()
+  
+  xup = equidistribution(length(labup))
+  
+  yup = rep(1,length(labup))
+  
+  d_nodes=data.frame(x=c(xdown,xup),
+                     y=c(ydown,yup),
+                     label = c(labdown,labup))
+  
+  
+  # Create a dataframe with links
+  
+  plant_positions <- data.frame(plant = labdown,
+                                xdown = xdown,
+                                ydown = ydown)
+  
+  pollinator_positions <- data.frame(pollinator = labup,
+                                     xup = xup,
+                                     yup = yup)
+  
+  link_df <- motif_data %>% left_join(plant_positions, by = "plant") %>% 
+    left_join(pollinator_positions, by = "pollinator")
+  
+  # Before plotting the motif, we remove letters from the node position id
+  
+  d_nodes$label <- gsub("[^0-9.-]", "", d_nodes$label)
+  plant_pies_aux <- plant_pies %>% select(-x,-y) %>% mutate(position = as.character(position))
+  
+  d_nodes_plant <- d_nodes %>% rename(position = label) %>% 
+    left_join(plant_pies_aux, by = "position") %>% as_tibble() %>%
+    filter(!is.na(`1`))
+  
+  pollinator_pies_aux <- pollinator_pies %>% select(-x,-y) %>% mutate(position = as.character(position))
+  
+  d_nodes_pollinator <- d_nodes %>% rename(position = label) %>% 
+    left_join(pollinator_pies_aux, by = "position") %>% as_tibble() %>%
+    filter(!is.na(Bee))
+  
+  # Define the number of colors you want
+  nb.cols <- 5
+  mycolors <- c(brewer.pal(5, "Dark2"),brewer.pal(9, "Paired"))
+  
+  ggplot() +
+    scale_shape_identity() +
+    geom_segment(data = link_df, aes(x=xdown, y=ydown, xend = xup, yend = yup), 
+                 size = 1, color = "grey20")+
+    geom_scatterpie(aes(x=x, y=y, group=position,r=0.3), 
+                    data = d_nodes_plant,
+                    cols=as.character(c("1","2","3","4","5")))+
+    geom_scatterpie(aes(x=x, y=y, group=position,r=.3), 
+                    data = d_nodes_pollinator,
+                    cols=as.character(c("Bee","Birds","Coleoptera","Lepidoptera",
+                                        "Lizards","Non-bee-Hymenoptera",
+                                        "Non-syrphids-diptera","Other_insects",
+                                        "Syrphids")))+
+    coord_equal()+
+    scale_fill_manual(values = mycolors)+
+    xlim(-1.5,4)+ ylim(-0.5,1.5)+
+    theme(panel.background = element_blank(),
+          axis.text.x=element_blank(),
+          axis.ticks.x=element_blank(),
+          axis.text.y=element_blank(),
+          axis.ticks.y=element_blank())+
+    labs(x=NULL,y=NULL)+
+    guides(color = FALSE)+
+    ggtitle(paste0("Motif ",motif_number))+
+    theme(plot.title = element_text(hjust = 0.5),
+          legend.position = "none")
+}
+
+motif_number <- 1
+p1 <- plot_pie_motif_positions(motif_number)
+
+motif_number <- 2
+p2 <- plot_pie_motif_positions(motif_number)
+
+motif_number <- 3
+p3 <- plot_pie_motif_positions(motif_number)
+
+motif_number <- 4
+p4 <- plot_pie_motif_positions(motif_number)
+
+motif_number <- 5
+p5 <- plot_pie_motif_positions(motif_number)
+
+motif_number <- 6
+p6 <- plot_pie_motif_positions(motif_number)
+
+motif_number <- 7
+p7 <- plot_pie_motif_positions(motif_number)
+
+motif_number <- 8   
+p8 <- plot_pie_motif_positions(motif_number)
+
+motif_number <- 9
+p9 <- plot_pie_motif_positions(motif_number)
+
+motif_number <- 10
+p10 <- plot_pie_motif_positions(motif_number)
+
+motif_number <- 11
+p11 <- plot_pie_motif_positions(motif_number)
+
+motif_number <- 12
+p12 <- plot_pie_motif_positions(motif_number)
+
+motif_number <- 13
+p13 <- plot_pie_motif_positions(motif_number)
+
+motif_number <- 14
+p14 <- plot_pie_motif_positions(motif_number)
+
+motif_number <- 15
+p15 <- plot_pie_motif_positions(motif_number)
+
+motif_number <- 16
+p16 <- plot_pie_motif_positions(motif_number)
+
+motif_number <- 17
+p17 <- plot_pie_motif_positions(motif_number)
+
+
+library(patchwork)
+
+(p1 & ylab("2 species")| p2 & ylab("3 species") | p3 )/ (p4 & ylab("4 species")| p5 | p6 | p7)/ (p8  & ylab("5 species")| p9 | p10 | p11 | p12) / (p13 & ylab("5 species")| p14 | p15 | p16 | p17) & theme(axis.title.y = element_text(color="black", size=14, face="bold")) #+
+#plot_annotation(tag_levels = '1') 
 

c7cf81d6af62f59266007fc07c910e5fc57c6ba0
Alfonso Allen-Perkins <alfonso.allen@hotmail.com>
2021-07-27 08:15:49 +0200

    Update Z-score_percentile_histogram_plot.R

--- Scripts/Z-score_percentile_histogram_plot.R
+++ Scripts/Z-score_percentile_histogram_plot.R
@@ -30,7 +30,42 @@ data_2 <- filter(data_1, z_score<3000)
 
 #Plot histogram of Z-scores
 ggplot(data_2, aes(x=z_score)) + 
-  geom_histogram(color="black", fill="white")
+  geom_histogram(color="black", fill="white",bins = 28)+
+  geom_vline(xintercept = -2)+
+  geom_vline(xintercept = 2)+
+  xlim(-50,50)
 
 
+###############
+# PERCENTILES
+###############
 
+data_perc <- as.tibble(data)
+
+# There are 827 motifs with upper and lower CI equal to zero
+data_perc %>% filter(lower_CI == upper_CI)
+data_perc %>% filter(lower_CI == upper_CI, lower_CI>0)
+
+data_perc$infra_over_represented <- "no_diff"
+data_perc$infra_over_represented[data_perc$round_motif_observed_probability<data_perc$lower_CI] <- "infra"
+data_perc$infra_over_represented[data_perc$round_motif_observed_probability>data_perc$upper_CI] <- "over"
+
+# There are 19514 motifs whose observed probability (0) is expected and equal to the lower bound (0)
+data_perc %>% filter(percentil_observed == 0, infra_over_represented == "no_diff")
+
+min(data_perc$percentil_observed[data_perc$infra_over_represented=="infra"])
+max(data_perc$percentil_observed[data_perc$infra_over_represented=="infra"])
+
+min(data_perc$percentil_observed[data_perc$infra_over_represented=="over"])
+max(data_perc$percentil_observed[data_perc$infra_over_represented=="over"])
+
+min(data_perc$percentil_observed[data_perc$infra_over_represented=="no_diff"])
+max(data_perc$percentil_observed[data_perc$infra_over_represented=="no_diff"])
+
+data_perc %>% group_by(infra_over_represented) %>% count()
+
+#Plot histogram of percentil
+ggplot(data, aes(x=percentil_observed)) + 
+  geom_histogram(color="black", fill="white",bins = 100)+
+  geom_vline(xintercept = 0.025)+
+  geom_vline(xintercept = 0.975)

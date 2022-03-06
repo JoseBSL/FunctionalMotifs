ee9e578d1d03701f759b9810eb1a5fe9af597cc0
Jose Barragán Sánchez-Lanuza <lanuza@Joses-iMac.local>
2022-03-06 20:05:25 +0100

    Preparing facets

--- Scripts/Direct_Indirect.R
+++ Scripts/Direct_Indirect.R
@@ -3,75 +3,52 @@ library(scatterpie)
 library(RColorBrewer)
 library(tidyverse)
 
+
+direct_indirect <- read_csv("Data/Data_processing/Motifs_connections/motif_interactions.csv")
+colnames(direct_indirect)
+direct_indirect <- direct_indirect %>% select(motif_id,position,direct_interactions, indirect_interactions,percentage_indirect_interactions)
+
 plant_means_reordered <- read_csv("Data/Csv/plant_abs_freq_means.csv")
 pollinator_means_reordered <- read_csv("Data/Csv/pollinator_abs_freq_means.csv")
 #Filter out in order to have just 6 FG'S
 pollinator_means_reordered <- pollinator_means_reordered %>% filter(!Node_FG %in% c("Birds", "Lizards", "Other_insects"))
 
+#Check pol levels
+levels(factor(pollinator_means_reordered$Node_FG))
 
-# Pollinator--------
-pollinator_trial <- pollinator_means_reordered %>% select(-mean, -SE)# %>% 
-  #spread(Node_FG,mean_natural_units)
+#Dplyr merge
+joined_tibble <- left_join(direct_indirect, pollinator_means_reordered)
 
-#Select potions of interest just for 3 species
-s <- c(seq(from=3, to=16))
-#filter dataset
-d <- pollinator_trial %>% filter(position %in% s) %>% filter(Node_FG=="Bee")
-d$indirect <- NA
-#Add number of indirect interactions manually
-#Faster for now, do not know how to code it easily
-
-#3species
-d$indirect[d$position=="3"] <- "1"
-d$indirect[d$position=="4"] <- "0"
-d$indirect[d$position=="5"] <- "0"
-d$indirect[d$position=="6"] <- "1"
-#4species
-d$indirect[d$position=="7"] <- "0"
-d$indirect[d$position=="8"] <- "2"
-d$indirect[d$position=="9"] <- "1"
-d$indirect[d$position=="10"] <- "1"
-d$indirect[d$position=="11"] <- "1"
-d$indirect[d$position=="12"] <- "1"
-d$indirect[d$position=="13"] <- "1"
-d$indirect[d$position=="14"] <- "1"
-d$indirect[d$position=="15"] <- "2"
-d$indirect[d$position=="16"] <- "0"
-#5species
-d$indirect[d$position=="17"] <- "0"
-d$indirect[d$position=="18"] <- "0"
-d$indirect[d$position=="19"] <- "0"
-d$indirect[d$position=="20"] <- "0"
-d$indirect[d$position=="21"] <- "0"
-d$indirect[d$position=="22"] <- "0"
-d$indirect[d$position=="23"] <- "0"
-d$indirect[d$position=="24"] <- "0"
-d$indirect[d$position=="25"] <- "0"
-d$indirect[d$position=="26"] <- "0"
-d$indirect[d$position=="27"] <- "0"
-d$indirect[d$position=="28"] <- "0"
-d$indirect[d$position=="29"] <- "0"
-d$indirect[d$position=="30"] <- "0"
-d$indirect[d$position=="31"] <- "0"
-d$indirect[d$position=="32"] <- "0"
-d$indirect[d$position=="33"] <- "0"
-d$indirect[d$position=="34"] <- "0"
-d$indirect[d$position=="35"] <- "0"
-d$indirect[d$position=="36"] <- "0"
-d$indirect[d$position=="37"] <- "0"
-d$indirect[d$position=="38"] <- "0"
-d$indirect[d$position=="39"] <- "0"
-d$indirect[d$position=="40"] <- "0"
-d$indirect[d$position=="41"] <- "0"
-d$indirect[d$position=="42"] <- "0"
-d$indirect[d$position=="43"] <- "0"
-d$indirect[d$position=="44"] <- "0"
-d$indirect[d$position=="45"] <- "0"
-
-
-
-
-d$position <- as.factor(d$position)
-ggplot(d,aes(position,mean_natural_units))+
+#Bees
+bees <- joined_tibble %>% filter(Node_FG=="Bee")
+bees$percentage_indirect_interactions <- round(bees$percentage_indirect_interactions, digits = 2)
+#Convert to factor
+bees$position <- as.factor(bees$position)
+bees$percentage_indirect_interactions <- as.factor(bees$percentage_indirect_interactions)
+bee_plot <- ggplot(bees,aes(position,mean_natural_units))+
   geom_col(position = "dodge")+
-  facet_grid(.~indirect,scale='free_x', space = "free_x") + ggtitle("Bees")
+  facet_grid(.~percentage_indirect_interactions,scale='free_x', space = "free_x") + ggtitle("Bees")
+
+#Coleoptera
+cole <- joined_tibble %>% filter(Node_FG=="Coleoptera")
+cole$percentage_indirect_interactions <- round(cole$percentage_indirect_interactions, digits = 2)
+#Convert to factor
+cole$position <- as.factor(cole$position)
+cole$percentage_indirect_interactions <- as.factor(cole$percentage_indirect_interactions)
+cole_plot <- ggplot(cole,aes(position,mean_natural_units))+
+  geom_col(position = "dodge")+
+  facet_grid(.~percentage_indirect_interactions,scale='free_x', space = "free_x") + ggtitle("Coleoptera")
+
+#Lepidoptera
+lepi <- joined_tibble %>% filter(Node_FG=="Lepidoptera")
+lepi$percentage_indirect_interactions <- round(lepi$percentage_indirect_interactions, digits = 2)
+#Convert to factor
+lepi$position <- as.factor(lepi$position)
+lepi$percentage_indirect_interactions <- as.factor(lepi$percentage_indirect_interactions)
+lepi_plot <- ggplot(lepi,aes(position,mean_natural_units))+
+  geom_col(position = "dodge")+
+  facet_grid(.~percentage_indirect_interactions,scale='free_x', space = "free_x") + ggtitle("Lepidoptera")
+
+
+library(patchwork)
+bee_plot / cole_plot / lepi_plot

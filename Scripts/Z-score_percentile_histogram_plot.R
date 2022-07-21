253267d60fd19a82ad8251b0f96de044895a150e
Jose Barragán Sánchez-Lanuza <barragansljose@gmail.comcd>
2022-07-21 10:58:56 +0200

    edits

--- Scripts/Z-score_percentile_histogram_plot.R
+++ Scripts/Z-score_percentile_histogram_plot.R
@@ -4,7 +4,7 @@ library(dplyr)
 library(ggplot2)
 library(tidyverse)
 #Read data
-data <- read.csv("../../Data/Csv/motifs_observed_probability_SIMUL_CI.csv")
+data <- read.csv("Data/Csv/motifs_observed_probability_SIMUL_CI.csv")
 
 #Check NA's per column
 #data %>%
@@ -13,6 +13,9 @@ data <- read.csv("../../Data/Csv/motifs_observed_probability_SIMUL_CI.csv")
 #Find critical value of Z-score
 p <- 0.05 #cutoff probability 95% confidence
 critical_value <- qnorm(p/2) #double tail probability divide by 2
+
+
+
 
 data_1 <- data %>%
   mutate(infra_over_represented = case_when(
@@ -40,7 +43,7 @@ data_2 <- filter(data_1, abs(z_score)<2000)
 # Podrian estar underrepresented, pero como con la Chi, si el expected es super bajo, 
 # diferenciar si esta under-represented sera dificil de detectar .
 
-ggplot(data_2 %>% filter(round_motif_observed_probability>0), aes(x=z_score, color=infra_over_represented, fill=infra_over_represented)) + 
+ggplot(data_1, aes(x=z_score, color=infra_over_represented, fill=infra_over_represented)) + 
   geom_histogram(bins = 100, alpha = 0.5, position = "identity",lwd = 0.25)+
   geom_vline(xintercept = -abs(critical_value))+
   geom_vline(xintercept = abs(critical_value))+

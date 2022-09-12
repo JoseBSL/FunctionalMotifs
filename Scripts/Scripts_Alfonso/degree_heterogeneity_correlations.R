4de8a905952204cb0ac7b61334ce65b06d1552aa
Alfonso Allen-Perkins <alfonso.allen@hotmail.com>
2022-09-12 21:34:48 +0200

    Update degree_heterogeneity_correlations.R

--- Scripts/Scripts_Alfonso/degree_heterogeneity_correlations.R
+++ Scripts/Scripts_Alfonso/degree_heterogeneity_correlations.R
@@ -47,41 +47,70 @@ gini_coeff_fun <- function(network_name){
   
 }
 
-randic_index_fun <- function(network_name){
+VAR_fun <- function(network_name){
   
   degree_sequence <- degree_sequence_from_edgelist(network_name)
-  output <- 0
   
-  for (i in 1:length(degree_sequence)) {
-    
-    for (j in 1:length(degree_sequence)) {
-      
-      output <- output + (degree_sequence[i]*degree_sequence[j])^(-0.5)
-      
-    }
-    
-  }
-  
-  return(output)
+  return(var(degree_sequence))
   
 }
+
+heterogeneity_index_fun <- function(network_name){
+  
+  networks_i <- networks %>% filter(Network_id == network_name)
+  
+  networks_i <- networks_i %>% 
+    group_by(Pollinator_species,Plant_species,
+             Pollinator_functional_group,Plant_functional_groups) %>%
+    count(wt = Interaction) %>% rename(Interaction = n)
+  
+  edge_list_i <- networks_i[,c("Pollinator_species","Plant_species")]
+  
+  plant_degree <- edge_list_i %>% ungroup() %>% dplyr::select(Plant_species) %>% 
+    dplyr::group_by(Plant_species) %>%
+    count() %>% rename(degree_Plant = n)
+  
+  plant_nodes <- nrow(plant_degree)
+  
+  pollinator_degree <- edge_list_i %>% ungroup() %>% dplyr::select(Pollinator_species) %>% 
+    dplyr::group_by(Pollinator_species) %>%
+    count() %>% rename(degree_Pollinator = n)
+  
+  pollinator_nodes <- nrow(pollinator_degree)
+  
+  total_nodes <- plant_nodes + pollinator_nodes
+  
+  edge_list_i_final <- edge_list_i %>% 
+    left_join(pollinator_degree, by = "Pollinator_species") %>%
+    left_join(plant_degree, by = "Plant_species") %>%
+    mutate(fun_degr_Poll = 1/sqrt(degree_Pollinator),
+           fun_degr_Plant = 1/sqrt(degree_Plant),
+           irregularity = ( fun_degr_Poll - fun_degr_Plant )^2)
+
+  return(sum(edge_list_i_final$irregularity)/(total_nodes-2*sqrt(total_nodes-1)))
+  
+}
+
 
 #Calculate metrics
 connectance = lapply(my_data, function(i) {networklevel(as.matrix(i), index="connectance")})
 degree_dist = lapply(my_data, function(i) {networklevel(as.matrix(i), index="links per species")})
 nestedness = lapply(my_data, function(i) {networklevel(as.matrix(i), index="nestedness")})
 gini_coefficients = lapply(my_network_ids, function(i) {gini_coeff_fun(i)})
-randic_index = lapply(my_network_ids, function(i) {randic_index_fun(i)})
+heterogeneity_index = lapply(my_network_ids, function(i) {heterogeneity_index_fun(i)})
+VAR = lapply(my_network_ids, function(i) {VAR_fun(i)})
 
 #Connectance/nestedness
 connectance_d = bind_rows(connectance)
 nestedness_d = bind_rows(nestedness)
 degree_dist_d = bind_rows(degree_dist)
 gini_dist_d = as.numeric(gini_coefficients)
-randic_index_d = as.numeric(randic_index)
+heterogeneity_index_dist_d = as.numeric(heterogeneity_index)
+VAR_dist_d = as.numeric(VAR)
 
 cor.test(connectance_d$connectance, nestedness_d$nestedness, method = "spearman")
 cor.test(connectance_d$connectance, degree_dist_d$`links per species`, method = "spearman")
 cor.test(connectance_d$connectance, nestedness_d$nestedness, method = "spearman")
 cor.test(connectance_d$connectance, gini_dist_d, method = "spearman")
-cor.test(connectance_d$connectance, randic_index_d, method = "spearman")
+cor.test(connectance_d$connectance, heterogeneity_index_dist_d, method = "spearman")
+cor.test(connectance_d$connectance, VAR_dist_d, method = "spearman")
